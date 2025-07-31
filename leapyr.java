d991800fcac2ceb98e6ee99c6620de755ee7f094
Marysujitha <160560464+Marysujitha@users.noreply.github.com>
2025-07-31 20:46:17 +0530

    Create leapyr.java

---
+++ leapyr.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if((n%4==0)&& (n%100!=0) || (n%400==0))
+            System.out.println("Leap year");
+        else
+            System.out.println("Not a Leap year");
+    }
+}
