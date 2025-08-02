c5490a31407fc674bf509738b99cfa7813dc3f00
Marysujitha <160560464+Marysujitha@users.noreply.github.com>
2025-08-02 23:03:40 +0530

    Create productofno.java
    Program to find the product of the digits in a given number

---
+++ productofno.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ld,mul=1;
+        while(n>0)
+        {
+            ld=n%10;
+            mul*=ld;
+            n=n/10;
+        }
+        System.out.print(mul);
+    }
+}
