15cad34518b01fa43876aa5793a63ea05d6651bc
Marysujitha <160560464+Marysujitha@users.noreply.github.com>
2025-08-03 21:15:39 +0530

    Create invertedtripattern.java
    inverted triangle pattern
    
    *******
      *****
       ***
         *

---
+++ invertedtripattern.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n,r,sp,st;
+        n=sc.nextInt();
+        for(r=n;r>=1;r--)
+        {
+            for(sp=1;sp<=n-r;sp++)
+                System.out.print(" ");
+            for(st=1;st<=2*r-1;st++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
