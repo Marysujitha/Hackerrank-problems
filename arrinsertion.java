8bd2d6bd414cce78347586e6228454b7f0e13fb0
Marysujitha <160560464+Marysujitha@users.noreply.github.com>
2025-08-11 20:33:04 +0530

    Create arrinsertion.java
    Write a program to accept the array and position in which the new element to be inserted from the user and display the array.

---
+++ arrinsertion.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n+1];
+        int d,pos,i;
+        for(i=0;i<n;i++)
+        {
+            a[i]=s.nextInt();
+        }
+        d=s.nextInt();
+        pos=s.nextInt();
+        for(i=n;i>pos-1;i--)
+        {
+            a[i]=a[i-1];
+        }
+        a[pos-1]=d;
+        for(i=0;i<n+1;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+}
